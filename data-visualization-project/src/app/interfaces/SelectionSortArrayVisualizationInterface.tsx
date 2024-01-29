f08f7c67cb32010a80de77accce2dbb11a3beae3
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-01-28 23:09:37 -0800

    "Fixed issues in selection sort"

--- data-visualization-project/src/app/interfaces/SelectionSortArrayVisualizationInterface.tsx
+++ data-visualization-project/src/app/interfaces/SelectionSortArrayVisualizationInterface.tsx
@@ -1,27 +1,33 @@
 import { IMarker } from "react-ace";
 
-export interface SelectionSortArrayVisualizationInterface{
-    value: number;
-    index: number;
-    color: string;
+export interface SelectionSortArrayVisualizationInterface {
+  value: number;
+  index: number;
+  color: string;
+  type: string;
 }
 
-export interface SelectionSortArrayVisualizationAnimationInterface{
-    valueI: number;
-    valueJ: number;
-    indexI: number;
-    indexJ: number;
-    colorI: string;
-    colorJ: string;
-    currentLineMarkers : IMarker[]; 
-    minValue: string;
-    minIndex: string
+export interface SelectionSortArrayVisualizationAnimationInterface {
+  valueI: number;
+  valueJ: number;
+  indexI: number;
+  indexJ: number;
+  colorI: string;
+  colorJ: string;
+  currentLineMarkers: IMarker[];
+  minValue: string;
+  minIndex: string;
 }
 
-const SelectionSortArrayVisualizationInterfaceComponent = ()=>{
-    return(
-        <div></div>
-    )
+export interface CountingSortArrayVisualizationAnimationInterface {
+  valueI: number;
+  indexI: number;
+  colorI: string;
+  type: string;
 }
+
+const SelectionSortArrayVisualizationInterfaceComponent = () => {
+  return <div></div>;
+};
 
 export default SelectionSortArrayVisualizationInterfaceComponent;
